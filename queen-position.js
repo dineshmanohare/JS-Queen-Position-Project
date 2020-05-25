79e884ec6eab0979577f9d0ccb080e6bcd020360
dineshmanohare <dineshmanohare@gmail.com>
2020-05-25 17:52:11 +0530

    recurssion done

--- queen-position.js
+++ queen-position.js
@@ -2,182 +2,197 @@ var row = [8, 7, 6, 5, 4, 3, 2, 1];
 var column = ["A", "B", "C", "D", "E", "F", "G", "H"];
 var queen = {
     position: {
-        row: 1,
-        column: 3
-    }
+        row: 2,
+        column: 4
+    },
 };
-var direction = window.prompt("enter in which direction queen have to move");
-var steps = window.prompt("enter how many steps queen have to move");
 
-switch (direction) {
-    case 'E': east(steps);
-        break;
-    case 'W': west(steps);
-        break;
-    case 'S': south(steps);
-        break;
-    case 'N': north(steps);
-        break;
-    case 'NE': northEast(steps);
-        break;
-    case 'SE': southEast(steps);
-        break;
-    case 'SW': southWest(steps);
-        break;
-    case 'NW': northWest(steps);
-        break;
+function getStart() {
+    var direction = window.prompt("enter in which direction queen have to move");
+    var steps = window.prompt("enter how many steps queen have to move");
 
+    if (direction != "") {
+        getDirection(direction, steps);
+    }
+
+
+    function getDirection(direction, steps) {
+        switch (direction) {
+            case 'E': east(steps);
+                break;
+            case 'W': west(steps);
+                break;
+            case 'S': south(steps);
+                break;
+            case 'N': north(steps);
+                break;
+            case 'NE': northEast(steps);
+                break;
+            case 'SE': southEast(steps);
+                break;
+            case 'SW': southWest(steps);
+                break;
+            case 'NW': northWest(steps);
+                break;
+
+        }
+    }
+
+    function east(steps) {
+        var column_result = queen.position.column + parseInt(steps);
+        var row_result = queen.position.row;
+
+        if (column_result <= 7) {
+            var row_array_result = row[row_result];
+            var column_array_result = column[column_result];
+            var position = row_array_result + column_array_result;
+            alert("queen position is " + position)
+        }
+        else {
+            alert("queen is going out side of board");
+        }
+
+        queen.position.row = row_result;
+        queen.position.column = column_result;
+    }
+
+    function west(steps) {
+        var column_result = queen.position.column - parseInt(steps);
+        var row_result = queen.position.row;
+
+        if (column_result >= 0) {
+            var row_array_result = row[row_result];
+            var column_array_result = column[column_result];
+            var position = row_array_result + column_array_result;
+            alert("queen position is " + position)
+        }
+        else {
+            alert("queen is going out side of board");
+        }
+
+        queen.position.row = row_result;
+        queen.position.column = column_result;
+
+    }
+
+    function south(steps) {
+        var column_result = queen.position.column;
+        var row_result = queen.position.row + parseInt(steps);
+        if (row_result <= 7) {
+            var row_array_result = row[row_result];
+            var column_array_result = column[column_result];
+            var position = row_array_result + column_array_result;
+            alert("queen position is " + position)
+        }
+        else {
+            alert("queen is going out side of board");
+        }
+        queen.position.row = row_result;
+        queen.position.column = column_result;
+    }
+
+    function north(steps) {
+        var column_result = queen.position.column;
+        var row_result = queen.position.row - parseInt(steps);
+        3
+        if (row_result >= 0) {
+            var row_array_result = row[row_result];
+            var column_array_result = column[column_result];
+            var position = row_array_result + column_array_result;
+            alert("queen position is " + position)
+        }
+        else {
+            alert("queen is going out side of board");
+        }
+
+        queen.position.row = row_result;
+        queen.position.column = column_result;
+    }
+
+    function northEast(steps) {
+        var column_result = queen.position.column + parseInt(steps);
+        var row_result = queen.position.row - parseInt(steps);
+
+        if (row_result >= 0 && column_result <= 7) {
+            var row_array_result = row[row_result];
+            var column_array_result = column[column_result];
+            var position = row_array_result + column_array_result;
+            alert("queen position is " + position)
+        }
+        else {
+            alert("queen is going out side of board");
+        }
+
+        queen.position.row = row_result;
+        queen.position.column = column_result;
+    }
+
+    function southEast(steps) {
+        var column_result = queen.position.column + parseInt(steps);
+        var row_result = queen.position.row + parseInt(steps);
+
+        if (row_result <= 7 && column_result <= 7) {
+            var row_array_result = row[row_result];
+            var column_array_result = column[column_result];
+            var position = row_array_result + column_array_result;
+            alert("queen position is " + position)
+        }
+        else {
+            alert("queen is going out side of board");
+        }
+
+        queen.position.row = row_result;
+        queen.position.column = column_result;
+    }
+
+    function southWest(steps) {
+        var column_result = queen.position.column - parseInt(steps);
+        var row_result = queen.position.row + parseInt(steps);
+
+        if (row_result <= 7 && column_result >= 0) {
+            var row_array_result = row[row_result];
+            var column_array_result = column[column_result];
+            var position = row_array_result + column_array_result;
+            alert("queen position is " + position)
+        }
+        else {
+            alert("queen is going out side of board");
+        }
+
+        queen.position.row = row_result;
+        queen.position.column = column_result;
+    }
+
+    function northWest(steps) {
+        var column_result = queen.position.column - parseInt(steps);
+        var row_result = queen.position.row - parseInt(steps);
+
+        if (row_result >= 0 && column_result >= 0) {
+            var row_array_result = row[row_result];
+            var column_array_result = column[column_result];
+            var position = row_array_result + column_array_result;
+            alert("queen position is " + position)
+        }
+        else {
+            alert("queen is going out side of board");
+        }
+
+        queen.position.row = row_result;
+        queen.position.column = column_result;
+    }
+
+    queen.loopContinueResult = window.prompt("do you want to continue yes/no ");
+
+    if (queen.loopContinueResult == 'yes') {
+        getStart();
+    }
 }
 
-function east(steps) {
-    var column_result = queen.position.column + parseInt(steps);
-    var row_result = queen.position.row;
-    if (column_result <= 7) {
-        var row_array_result = row[row_result];
-        var column_array_result = column[column_result];
-        var position = row_array_result + column_array_result;
-        document.write(position);
-    }
-    else {
-        document.write("queen is going out side of board");
-    }
-    queen.position.row = row_result;
-    queen.position.column = column_result;
+window.onload = function () {
+    getStart();
 }
 
 
-function west(steps) {
-    var column_result = queen.position.column - parseInt(steps);
-    var row_result = queen.position.row;
-    if (column_result >= 0) {
-        var row_array_result = row[row_result];
-        var column_array_result = column[column_result];
-        var position = row_array_result + column_array_result;
-        document.write(position);
-
-
-    }
-    else {
-        document.write("queen is going out side of board");
-    }
-    queen.position.row = row_result;
-    queen.position.column = column_result;
-
-}
-function south(steps) {
-    var column_result = queen.position.column;
-    var row_result = queen.position.row + parseInt(steps);
-    if (column_result <= 7) {
-        var row_array_result = row[row_result];
-        var column_array_result = column[column_result];
-        var position = row_array_result + column_array_result;
-        document.write(position);
-
-
-    }
-    else {
-        document.write("queen is going out side of board");
-    }
-    queen.position.row = row_result;
-    queen.position.column = column_result;
-
-}
-function north(steps) {
-    var column_result = queen.position.column;
-    var row_result = queen.position.row - parseInt(steps);
-    if (row_result >= 0) {
-        var row_array_result = row[row_result];
-        var column_array_result = column[column_result];
-        var position = row_array_result + column_array_result;
-        document.write(position);
-
-
-    }
-    else {
-        document.write("queen is going out side of board");
-    }
-    queen.position.row = row_result;
-    queen.position.column = column_result;
-
-}
-
-function northEast(steps) {
-    var column_result = queen.position.column + parseInt(steps);
-    var row_result = queen.position.row - parseInt(steps);
-    if (row_result >= 0 && column_result <= 7) {
-        var row_array_result = row[row_result];
-        var column_array_result = column[column_result];
-        var position = row_array_result + column_array_result;
-        document.write(position);
-
-
-    }
-    else {
-        document.write("queen is going out side of board");
-    }
-    queen.position.row = row_result;
-    queen.position.column = column_result;
-
-}
-
-function southEast(steps) {
-    var column_result = queen.position.column + parseInt(steps);
-    var row_result = queen.position.row + parseInt(steps);
-    if (row_result <= 7 && column_result <= 7) {
-        var row_array_result = row[row_result];
-        var column_array_result = column[column_result];
-        var position = row_array_result + column_array_result;
-        document.write(position);
-
-
-    }
-    else {
-        document.write("queen is going out side of board");
-    }
-    queen.position.row = row_result;
-    queen.position.column = column_result;
-
-}
-
-
-function southWest(steps) {
-    var column_result = queen.position.column - parseInt(steps);
-    var row_result = queen.position.row + parseInt(steps);
-    if (row_result <= 7 && column_result >= 0) {
-        var row_array_result = row[row_result];
-        var column_array_result = column[column_result];
-        var position = row_array_result + column_array_result;
-        document.write(position);
-
-
-    }
-    else {
-        document.write("queen is going out side of board");
-    }
-    queen.position.row = row_result;
-    queen.position.column = column_result;
-
-}
-
-
-function northWest(steps) {
-    var column_result = queen.position.column - parseInt(steps);
-    var row_result = queen.position.row - parseInt(steps);
-    if (row_result >= 0 && column_result >= 0) {
-        var row_array_result = row[row_result];
-        var column_array_result = column[column_result];
-        var position = row_array_result + column_array_result;
-        document.write(position);
-
-
-    }
-    else {
-        document.write("queen is going out side of board");
-    }
-    queen.position.row = row_result;
-    queen.position.column = column_result;
-
-}
 
 
 
